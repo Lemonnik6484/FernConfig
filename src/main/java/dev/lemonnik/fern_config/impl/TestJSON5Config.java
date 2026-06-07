04daf3d656263bafa7134c0812b88cb6e15bcba2
lemonnik <lemongamer6484@gmail.com>
2026-06-07 19:19:11 +0200

    more readable examples

--- src/main/java/dev/lemonnik/fern_config/impl/TestJSON5Config.java
+++ src/main/java/dev/lemonnik/fern_config/impl/TestJSON5Config.java
@@ -29,6 +29,17 @@ public class TestJSON5Config extends CConfig {
 
     private static final CCategory COMPLEX_THINGS = CCategory.of("complex_category", "Unbasic values", "read carefully");
 
-    public final CEnum<MaskType> block_mask_type = register(COMPLEX_THINGS, new CEnum<MaskType>("mask_type", "This is enum that contains type of the mask below", MaskType.class, MaskType.WHITELIST));
-    public final CMask block_mask_itself = register(COMPLEX_THINGS, new CMask("block_mask", "This is mask itself that requires CEnum-of-MaskType's key to be created and config to access that enum -> ", "mask_type", this, "*:*"));
+    public final CEnum<MaskType> block_mask_type = register(COMPLEX_THINGS, new CEnum<MaskType>(
+            "mask_type",
+            "This is enum that contains type of the mask below",
+            MaskType.class,
+            MaskType.WHITELIST)
+    );
+    public final CMask block_mask_itself = register(COMPLEX_THINGS, new CMask(
+            "block_mask",
+            "This is mask itself that requires CEnum-of-MaskType's key to be created and config to access that enum -> ",
+            "mask_type",
+            this,
+            "*:*")
+    );
 }
